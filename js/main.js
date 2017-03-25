e938ad2ff4cf0cb339461848e927e705d9067263
Vitaliy <Vitalik-Shulgin@yandex.ru>
2017-03-25 14:05:04 +0200

    v2

--- js/main.js
+++ js/main.js
@@ -5,6 +5,25 @@ $(document).ready(function(){
 		$('.datalist').slideUp('slow');
 	})
 	
+	$('.main_slider').slick({
+		infinite: true,
+		slidesToShow: 1,
+		autoplay: true,
+		autoplaySpeed: 4000,
+		slidesToScroll: 1,
+		dots: true
+	});
+	
+	
+	$('.product_slider_content').slick({
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		dots: false,
+		prevArrow: '<button> <i class="fa fa-angle-left" aria-hidden="true"></i> Назад</button>',
+		nextArrow: '<button class="slick-next"> <i class="fa fa-angle-right" aria-hidden="true"></i> Вперед</button>'
+	});
+	
 	$('.news_slider').slick({
 		infinite: true,
 		slidesToShow: 1,
@@ -28,4 +47,70 @@ $(document).ready(function(){
 		dots: false,
 		vertical: true
 	});
+	
+	
+	//range
+	range_min =  Number($( ".range" ).attr('data-min'));
+	range_max =  Number($( ".range" ).attr('data-max'));
+	 $( "#slider-range" ).slider({
+    	range: true,
+    	min: range_min,
+    	max: range_max,
+    	values: [ 75, 300 ],
+    	slide: function( event, ui ) {
+		$( "#range_v1" ).val( ui.values[ 0 ] );
+			$( "#range_v2" ).val( ui.values[ 1 ] );
+		}
+    });
+    $( "#range_v1" ).val( $( "#slider-range" ).slider( "values", 0 ) );
+    $( "#range_v2" ).val( $( "#slider-range" ).slider( "values", 1 ) );
+	//end range
+	
+	//select
+	$('select').selectmenu();
+	
+	//end select
+	
+	//filtr
+	$('.filtr_all .viev_news').click(function(){
+		$(this).find('i').toggleClass('fa-long-arrow-up');
+		$('.hide_filtr').slideToggle('slow');
+		if ($(this).hasClass('hiden')) {
+			$(this).removeClass('hiden').find('b').text('показать больше фильтров');
+		}
+		else{
+			$(this).addClass('hiden').find('b').text('скрыть фильтры');
+		}
+		return false;
+	});
+	
+	//end filtr
+	
+	$('.sidebar_listing_nav .viev_news').click(function(){
+		$(this).find('i').toggleClass('fa-long-arrow-up');
+		$('.lick_hiden').slideToggle('slow');
+		if ($(this).hasClass('hiden')) {
+			$(this).removeClass('hiden').find('b').text('показать больше ');
+		}
+		else{
+			$(this).addClass('hiden').find('b').text('скрыть ');
+		}
+		return false;
+	});
+	
+	$('.product_slider_main .you_viev ul li').hover(
+	function(){
+	  $('.product_slider_main .slick-list').height(600);
+	},
+	function(){
+	  $('.product_slider_main .slick-list').height(480);
+	});
+	
+	
+	$('.catalog_nav h3').click(function(){
+		$('.catalog_nav ul').slideToggle('slow');
+		$(this).toggleClass('active');
+	});
+	
+	
 })
